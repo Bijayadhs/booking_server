53d403f72aa5c7900d825e9111efcf0bfe655bfd
Bijayadhs <bijayadhikari107@gmail.com>
2021-02-09 23:29:02 +1100

    First commit express server routes and controller

---
+++ controllers/authController.js
@@ -0,0 +1,4 @@
+export const register = (req, res) => {
+    // res.status(200).json(req.body)
+    console.log(req.body);
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { readdirSync } from 'fs';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+const morgan = require('morgan');
+
+const app = express();
+dotenv.config();
+// require('dotenv').config();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+//database mongoose connect
+mongoose.connect(process.env.DATABASE, { useUnifiedTopology: true }).then(() => console.log('Database connected')).catch(err => console.log('Database Error: ', err));
+
+//routes middleware
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
+// app.use('/api', authRoute)
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is runnung in port ${PORT}`));
